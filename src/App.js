a85968123ebb9a44b2da1063bb00d07df50b932a
Hamza Ashraf <ashrah4@mcmaster.ca>
2023-07-07 15:39:56 -0400

    Third section completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Toolbar from "./components/Toolbar";
 import MainDisplay from "./components/MainDisplay";
 import WhoWeAre from "./components/WhoWeAre";
+import WhyClay from "./components/WhyClay";
+
 const App = () => {
   return (
     <div className="primary-container">
       <Toolbar />
       <MainDisplay />
       <WhoWeAre />
+      <WhyClay />
     </div>
   );
 };
---
+++ src/components/WhyClay.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const WhyClay = () => {
+  return (
+    <div className="d-container why-clay">
+      <div className="wc-container">
+        <div className="wc-text-1">Why Clay</div>
+        <div className="wc-text-2">The pre-construction experience is evolving, and so should you.</div>
+        <div className="wc-text-3">
+          Clay has changed the way people buy homes. From anywhere in the world, homebuyers and builders can connect and form
+          trusting relationships as if they were together in person.
+        </div>
+        <div className="wc-text-3">
+          Builders can virtualize projects and give homebuyers greater freedom with an unparalleled high-fidelity experience.
+          Clay's robust ecosystem is designed for builders to succeed in every project. It's so much more than a real-time
+          platform, it's a platform of opportunity to deliver more dreams to more people.
+        </div>
+        <button className="wc-btn primary-btn">Begin Your Experience</button>
+      </div>
+    </div>
+  );
+};
+
+export default WhyClay;
